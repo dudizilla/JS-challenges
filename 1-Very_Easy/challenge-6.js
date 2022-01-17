5a8ce3fa9e81816c7ed2d62f65610518ca8ae920
dudizilla <eduarda.mcunha@outlook.com>
2022-01-17 20:01:45 -0300

    challenge 6 tests

--- 1-Very_Easy/challenge-6.js
+++ 1-Very_Easy/challenge-6.js
@@ -5,3 +5,7 @@ function invertWords(word) {
 function invertWords(str) {
   return str.split(' ').map(invertWord).join(' ')
 }
+console.log(invert('Lorem ipsum dolore sec avanti')) // merol muspi erolod ces itnava
+console.log(invert('This is an apple')) //siht si na elppa
+console.log(invert('May the force be with you')) //yam eht ecrof eb htiw uoy
+console.log(invert('Its over nine thousand')) //sti revo enin dnasuoht
