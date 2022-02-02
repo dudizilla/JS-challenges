6eeb4d80184608df16101c1b4130b1d9247de83a
Eduarda Cunha <dudizilla@outlook.com>
2022-02-02 19:42:30 -0300

    challenge 23 done

--- 4-Hard/challenge-23.js
+++ 4-Hard/challenge-23.js
@@ -10,3 +10,71 @@
 
     Assuma que sempre receberá uma entrada válida no formato "Corredor AÇÃO"
 */
+
+class Race {
+  constructor(...racers) {
+    this.racers = racers
+  }
+
+  getClassification() {
+    const classification = {}
+
+    for (let i = 0; i < this.racers.length; i++) {
+      classification[i + 1] = this.racers[i];
+    }
+
+    return classification
+  }
+
+  updateClassification(str) {
+    const [racer, action] = str.split(' ')
+
+    if (action.includes('ELIMINATE')) {
+      this.eliminateRacer(racer)
+    } else {
+      this.updateRacer(racer, action)
+    }
+  }
+
+  updateRacer(racer, action) {
+    const eliminated = this.racers.filter(racer => racer.includes('ELIMINATED'))
+    const active = this.racers.filter(racer => !racer.includes('ELIMINATED'))
+
+    const currentPosition = active.indexOf(racer)
+    const updatedPosition = currentPosition + Number(action) * -1
+
+    if (updatedPosition >= this.racers.length || updatedPosition < 0) {
+      console.log('Error! Invalid action.')
+      return
+    }
+
+    active.splice(currentPosition, 1)
+
+    this.racers = [...active.slice(0, updatedPosition), racer, ...active.slice(updatedPosition), ...eliminated]
+  }
+
+  eliminateRacer(racer) {
+    const eliminated = this.racers.filter(racer => racer.includes('ELIMINATED'))
+    const active = this.racers.filter(racer => !racer.includes('ELIMINATED'))
+
+    active.splice(active.indexOf(racer), 1)
+
+    this.racers = [...active, `${racer} ELIMINATED`, ...eliminated]
+  }
+}
+
+const race1 = new Race("Alfa", "Beta", "Gama", "Delta") 
+
+race1.updateClassification("Beta +1")
+console.log(race1)
+
+race1.updateClassification("Gama -1")
+console.log(race1)
+
+race1.updateClassification("Delta ELIMINATE")
+console.log(race1)
+
+race1.updateClassification("Gama +2")
+console.log(race1)
+
+console.log(race1.getClassification())
