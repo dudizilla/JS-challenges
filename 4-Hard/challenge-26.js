5d5b83933695d851924dae26935f0f25b10ff458
Eduarda Cunha <dudizilla@outlook.com>
2022-02-03 13:34:29 -0300

    challenge 26 done

--- 4-Hard/challenge-26.js
+++ 4-Hard/challenge-26.js
@@ -4,3 +4,37 @@
 
   A função deve ser capaz de funcionar em números grandes como 10000 e retornar números inteiros corretos.
 */
+
+let fibonacciCache = {}
+
+function fibonacci(num) {
+  const big = BigInt(num)
+
+  if (typeof fibonacciCache[big] === 'bigint') {
+    return fibonacciCache[big]
+  }
+
+  let result = 0n
+
+  if (big === 0n) {
+    result = 0n
+  } else if (big === 1n) {
+    result = 1n
+  } else if (big >= 2n) {
+    result = fibonacci(big - 1n) + fibonacci(big - 2n)
+  }
+
+  fibonacciCache[big] = result
+
+  return result
+}
+
+//teste processamento até o num. 10000
+// for (let i = 0; i < 10000 ; i++) {
+//   console.log(i,fibonacci(i), '\n')
+// } 
+
+console.log(fibonacci(0))    //0n
+console.log(fibonacci(2n))   //1n
+console.log(fibonacci(7))    //13n
+console.log(fibonacci(144n)) //555565404224292694404015791808n
