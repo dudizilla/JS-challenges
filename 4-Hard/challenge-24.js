5cfced4086d0a75a84e489c365bf52a6459506b7
Eduarda Cunha <dudizilla@outlook.com>
2022-02-03 12:37:25 -0300

    challenge 24 done

--- 4-Hard/challenge-24.js
+++ 4-Hard/challenge-24.js
@@ -9,3 +9,48 @@
     como um array contendo vários nomes fictícios para comparação, por exemplo);
   -Caso atenda, retorne true, caso não atenda, retorne false.
 */
+
+const database = ['erick_14', 'pam_ls2', 'VICTOR_99A']
+
+function validateUser(username) {
+  if (username.length < 4 || username.length > 32) {
+    console.log('Tamanho inválido.\n')
+    return false
+  }
+
+  if (username.match(/\W/)) {
+    console.log('Caracteres inválidos.\n')
+    return false
+  }
+
+  if (username.match(/^[0-9]|^_/)) {
+    console.log('Caracteres inválidos.\n')
+    return false
+  }
+
+  if (username.match(/_$/)) {
+    console.log('Caracteres inválidos.\n')
+    return false
+  }
+
+  if (! (username.match(/[A-Za-z]/) && username.match(/[0-9]/) && username.match(/_/)) ) {
+    console.log('É necessário um de cada tipo de caractere.\n')
+    return false
+  }
+
+  const usernameAlreadyExists = database.indexOf(username) !== -1
+
+  if (usernameAlreadyExists) {
+    console.log('Nome de usuário já existe.\n')
+    return false
+  }
+
+  return true
+}
+
+console.log(validateUser('52alfred')) //false
+console.log(validateUser('erick_14')) //false
+console.log(validateUser('josh_g15')) //true
+console.log(validateUser('hugo123_')) //false
+console.log(validateUser('k_9')) //false
+
